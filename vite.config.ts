a50f91cdf06d03ba8bf7142abebcf5ae2eeaf2a6
Igor Wessel <igor_wessel@hotmail.com>
2022-05-08 15:49:14 -0300

    chore: init repo

---
+++ vite.config.ts
@@ -0,0 +1,40 @@
+import path from 'path'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tsconfigPaths from 'vite-tsconfig-paths'
+
+import packageJson from './package.json'
+
+const getPackageName = () => {
+  return packageJson.name
+}
+
+const getPackageNameCamelCase = () => {
+  try {
+    return getPackageName().replace(/-./g, char => char[1].toUpperCase())
+  } catch (err) {
+    throw new Error('Name property in package.json is missing.')
+  }
+}
+
+const fileName = {
+  es: `${getPackageName()}.mjs`,
+  cjs: `${getPackageName()}.cjs`,
+}
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      name: getPackageNameCamelCase(),
+      formats: ['es', 'cjs'],
+      fileName: format => fileName[format],
+    },
+    rollupOptions: {
+      external: ['react', 'next/router', 'next/link'],
+      output: {},
+    },
+  },
+})
