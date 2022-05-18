01d4d52aadd9de373b711277bbb7e17c0e2d94be
Igor Wessel <igor_wessel@hotmail.com>
2022-05-18 08:09:07 -0300

    chore: build config

--- vite.config.ts
+++ vite.config.ts
@@ -35,37 +35,12 @@ export default defineConfig({
   plugins: [react(), tsconfigPaths()],
   build: {
     lib: {
-      entry: path.resolve(__dirname, 'src/breadcrumb.client.tsx'),
+      entry: path.resolve(__dirname, 'src/index.ts'),
       name: getPackageNameCamelCase(),
       formats: ['es', 'cjs'],
       fileName: format => fileName[format],
     },
     rollupOptions: {
-      input: {
-        [`${getPackageName()}.client`]: path.resolve(
-          __dirname,
-          'src/breadcrumb.client.tsx'
-        ),
-        [`${getPackageName()}.server`]: path.resolve(
-          __dirname,
-          'src/breadcrumb.server.tsx'
-        ),
-      },
-      output: [
-        {
-          entryFileNames: ({ facadeModuleId }) =>
-            getESMOutputFileName(facadeModuleId),
-          format: 'esm',
-          dir: path.resolve(__dirname, 'dist'),
-        },
-        {
-          entryFileNames: ({ facadeModuleId }) =>
-            getCJSOutputFileName(facadeModuleId),
-          format: 'commonjs',
-          exports: 'named',
-          dir: path.resolve(__dirname, 'dist'),
-        },
-      ],
       external: ['react', 'next/router', 'next/link'],
     },
   },
